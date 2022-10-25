8151ab556cbf233b77cebff99ecfd5be8cdaa83f
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-10-25 19:09:59 +0800

    Individual clip components and edit clips

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,9 @@
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ClipService } from 'src/app/services/clip.service';
+import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-manage',
@@ -8,15 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+  clips: IClip[] = [];
+  activeClip: IClip | null = null;
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
   ) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params.sort === '2' ? params.sort: '1'
+    });
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = [];
+      docs.forEach(doc => {
+          this.clips.push({
+            docID: doc.id,
+            ...doc.data()
+          })
+      });
     });
   }
 
@@ -28,5 +45,13 @@ export class ManageComponent implements OnInit {
         sort: value
       }
     })
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault();
+
+    this.activeClip = clip;
+
+    this.modal.toggleModal('editClip');
   }
 }
